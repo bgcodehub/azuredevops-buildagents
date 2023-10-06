3a8a06a4029ca532c36746cc1cca30c8444528e2
Brian Galeano <brian.galeano@profisee.com>
2023-10-06 14:00:53 -0400

    Add initial stage to install PowerShell Core on agents

--- scripts/install-pwsh.ps1
+++ scripts/install-pwsh.ps1
@@ -1,2 +1,10 @@
-sudo apt-get update
-sudo apt-get install -y powershell
+# Determine OS
+$isLinux = $PSVersionTable.Platform -eq "Unix"
+
+if ($isLinux) {
+    # Update packages and install PowerShell Core
+    /usr/bin/env bash -c "sudo apt-get update && sudo apt-get install -y powershell"
+} else {
+    # Install on Windows (Using a package manager like Chocolatey)
+    choco install powershell-core -y
+}
